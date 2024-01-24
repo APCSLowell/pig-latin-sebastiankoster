1ecb3da2e25ea6fb9ca10966fba5992769ae7c04
sebastiankoster <142959691+sebastiankoster@users.noreply.github.com>
2024-01-24 11:23:03 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -48,7 +48,7 @@ public class PigLatin {
 	    else if (sWord.substring(0,2).equals("qu"))
 		    return sWord.substring(2,sWord.length())+"quay";
 	    else if (findFirstVowel(sWord)>0){
-		    return sWord.substring(1,sWord.length())+sWord.substring(0,1)+"ay";
+		    return sWord.substring(findFirstVowel(sWord),sWord.length())+sWord.substring(0,findFirstVowel(sWord))+"ay";
 	    }
 	    else {
 		return "ERROR!";
